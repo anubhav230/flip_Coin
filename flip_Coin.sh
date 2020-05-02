5faf47b8b10644bac3d38a3fe52af3b7809a478a
anubhav230 <anubhavsingh230@gmail.com>
2020-05-02 15:06:15 +0530

    haid/tail win status

--- flip_Coin.sh
+++ flip_Coin.sh
@@ -1,9 +1,30 @@
 #!/bin/bash -x
-coin=$(( RANDOM %2 + 1 ));
-if [ $coin -eq 1 ];
-then
-	echo "heads win";
-else
-	echo "tails win";
-fi
+heads=0
+tails=1
+headCount=0
+tailCount=0
+for (( i=1; i>0; i++ ))
+do
+
+	coin=$((RANDOM%2));
+	if [ $coin -eq 1 ];
+	then
+		headCount=$(( $headCount + 1 ))
+		if [ $headCount -eq 11 ]
+		then
+			echo "heads win";
+			exit
+		fi
+	fi
+
+	if [ $coin -eq 0 ];
+        then
+               	tailCount=$(( $tailCount + 1 ))
+                if [ $tailCount -eq 11 ]
+		then
+                	echo "tails win";
+                	exit
+		fi
+        fi
+done
 
