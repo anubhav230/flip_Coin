e46ffa390b9f2eb522886643eb6f6bf3e929954f
anubhav230 <anubhavsingh230@gmail.com>
2020-05-03 22:04:22 +0530

    coin flip win check

--- flip_Coin.sh
+++ flip_Coin.sh
@@ -1,30 +1,23 @@
-#!/bin/bash -x
+#!/bin/bash
 heads=0
 tails=1
 headCount=0
 tailCount=0
-for (( i=1; i>0; i++ ))
+for (( i=1; i<=20; i++ ))
 do
 
 	coin=$((RANDOM%2));
 	if [ $coin -eq 1 ];
 	then
 		headCount=$(( $headCount + 1 ))
-		if [ $headCount -eq 11 ]
-		then
-			echo "heads win";
-			exit
-		fi
 	fi
 
 	if [ $coin -eq 0 ];
         then
                	tailCount=$(( $tailCount + 1 ))
-                if [ $tailCount -eq 11 ]
-		then
-                	echo "tails win";
-                	exit
-		fi
         fi
 done
+
+echo "heads is $headCount"
+echo "tails is $tailCount"
 
